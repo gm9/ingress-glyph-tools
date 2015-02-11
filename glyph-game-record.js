9933c4ccdbed6d54fb559bdce2d9bb3106ec8baf
gm9 <ko4akiyama@gmail.com>
2015-02-11 17:40:02 +0900

    ハック成績記録機能を追加した。

---
+++ glyph-game-record.js
@@ -0,0 +1,477 @@
+/*
+ * Copyright (c) 2015 gm9
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+//require glyph-game.js
+
+(function(){
+    var igt = gm9.IngressGlyphTools;
+    var WORST_TIME = 99999;
+
+    // Export:
+    igt.glyphGameRecord = {
+        // Functions
+        load: load,
+        save: save,
+        updateCurrentTime: updateCurrentTime,
+        addGlyphGameResult: addGlyphGameResult,
+        addHackResult: addHackResult,
+        getSequenceRecord: getSequenceRecord,
+        getGlyphRecord: getGlyphRecord,
+        exportRecord: exportRecord,
+        importRecord: importRecord,
+
+        // Classes
+        SequenceRecord: SequenceRecord,
+        GlyphRecord: GlyphRecord,
+
+        // Constants
+        WORST_TIME: WORST_TIME
+    };
+
+
+
+    function addGlyphGameResult(level, glyphCodes, result)
+    {
+        addHackResult(level, glyphCodes,
+                      result.correctArray, result.time, result.glyphInputTimes);
+    }
+
+    function addHackResult(level, glyphCodes, arrCorrect, time, timeGlyphs)
+    {
+        addSequenceRecord(level, glyphCodes, arrCorrect, time);
+
+        for(var gi = 0; gi < arrCorrect.length; ++gi){
+            addGlyphRecord(glyphCodes[gi], arrCorrect[gi], timeGlyphs[gi]);
+        }
+    }
+
+    /**
+     * グリフシーケンス毎の成績を表すクラス。
+     */
+    function SequenceRecord()
+    {
+        this.numHacks = 0; //ハック回数
+        this.numCorrectGlyphs = 0; //正解した総グリフ数
+        this.numFullyCorrectSequences = 0; //全問正解したシーケンスの総数
+        this.timeFullyCorrectSequences = 0; //全問正解した場合のハック時間の合計
+        this.numSpeedBonus = 0; //スピードボーナス総数
+        this.bestTime = WORST_TIME; //全問正解した場合のハック時間の最短値
+    }
+    SequenceRecord.prototype = {
+        getAverageCorrectGlyphs: function(){
+            return this.numCorrectGlyphs / this.numHacks;
+        },
+        getFullyCorrectRate: function(){
+            return this.numFullyCorrectSequences / this.numHacks;
+        },
+        getAverageFullyCorrectHackTime: function(){
+            return this.numFullyCorrectSequences > 0 ? this.timeFullyCorrectSequences / this.numFullyCorrectSequences : WORST_TIME;
+        },
+        getSpeedBonusRate: function(){
+            return this.numSpeedBonus / this.numHacks;
+        },
+        clone: function(){
+            var rec = new SequenceRecord();
+            rec.numHacks = this.numHacks;
+            rec.numCorrectGlyphs = this.numCorrectGlyphs;
+            rec.numFullyCorrectSequences = this.numFullyCorrectSequences;
+            rec.timeFullyCorrectSequences = this.timeFullyCorrectSequences;
+            rec.numSpeedBonus = this.numSpeedBonus;
+            rec.bestTime = this.bestTime;
+            return rec;
+        },
+        addRecord: function(rec){
+            if(rec){
+                this.numHacks += rec.numHacks;
+                this.numCorrectGlyphs += rec.numCorrectGlyphs;
+                this.numFullyCorrectSequences += rec.numFullyCorrectSequences;
+                this.timeFullyCorrectSequences += rec.timeFullyCorrectSequences;
+                this.numSpeedBonus += rec.numSpeedBonus;
+                this.bestTime = Math.min(this.bestTime, rec.bestTime);
+            }
+            return this;
+        },
+        toString: function(){
+            return this.numHacks + " " +
+                this.numCorrectGlyphs + " " +
+                this.numFullyCorrectSequences + " " +
+                this.timeFullyCorrectSequences + " " +
+                this.numSpeedBonus + " " +
+                this.bestTime;
+        }
+    };
+    SequenceRecord.fromHackResult = function(numCorrectGlyphs, time, level){
+        var rec = new SequenceRecord();
+        rec.numHacks = 1;
+        rec.numCorrectGlyphs += numCorrectGlyphs;
+        var fullyCorrect = (numCorrectGlyphs == igt.glyphGame.getLevelGlyphCount(level));
+        if(fullyCorrect){
+            ++rec.numFullyCorrectSequences;
+            rec.timeFullyCorrectSequences += time;
+            rec.bestTime = time;
+        }
+        if(fullyCorrect && time < igt.glyphGame.getLevelSpeedBonusTime(level)){ ///@todo Make a function check speed bonus(< or <= ?)
+            ++rec.numSpeedBonus;
+        }
+        return rec;
+    };
+    SequenceRecord.fromString = function(str){
+        var rec = new SequenceRecord();
+        var arr = str.split(" ");
+        rec.numHacks = parseInt(arr[0]);
+        rec.numCorrectGlyphs = parseInt(arr[1]);
+        rec.numFullyCorrectSequences = parseInt(arr[2]);
+        rec.timeFullyCorrectSequences = parseInt(arr[3]);
+        rec.numSpeedBonus = parseInt(arr[4]);
+        rec.bestTime = parseInt(arr[5]) || WORST_TIME;
+
+        if(rec.numHacks >= 0 &&
+           rec.numCorrectGlyphs >= 0 &&
+           rec.numFullyCorrectSequences >= 0 && rec.numFullyCorrectSequences <= rec.numHacks &&
+           rec.timeFullyCorrectSequences >= 0 &&
+           rec.numSpeedBonus >= 0 && rec.numSpeedBonus <= rec.numHacks &&
+           rec.bestTime >= 0 && rec.bestTime <= WORST_TIME){
+            return rec;
+        }
+        else{
+            return null;
+        }
+    };
+
+    /**
+     * グリフ毎の成績を表すクラス。
+     */
+    function GlyphRecord()
+    {
+        this.numHacks = 0;
+        this.numCorrectHacks = 0;
+        this.timeCorrectHacks = 0;
+        this.bestTime = WORST_TIME;
+    }
+    GlyphRecord.prototype = {
+        getAverageCorrect: function(){
+            return this.numCorrectHacks / this.numHacks;
+        },
+        getAverageCorrectHackTime: function(){
+            return this.numCorrectHacks > 0 ? this.timeCorrectHacks / this.numCorrectHacks : WORST_TIME;
+        },
+        clone: function(){
+            var rec = new GlyphRecord();
+            rec.numHacks = this.numHacks;
+            rec.numCorrectHacks = this.numCorrectHacks;
+            rec.timeCorrectHacks = this.timeCorrectHacks;
+            rec.bestTime = this.bestTime;
+            return rec;
+        },
+        addRecord: function(rec){
+            if(rec){
+                this.numHacks += rec.numHacks;
+                this.numCorrectHacks += rec.numCorrectHacks;
+                this.timeCorrectHacks += rec.timeCorrectHacks;
+                this.bestTime = Math.min(this.bestTime, rec.bestTime);
+            }
+            return this;
+        },
+        toString: function(){
+            return this.numHacks + " "+
+                this.numCorrectHacks + " "+
+                this.timeCorrectHacks + " "+
+                this.bestTime;
+        }
+    };
+    GlyphRecord.fromHackResult = function(correct, time){
+        var rec = new GlyphRecord();
+        ++rec.numHacks;
+        if(correct){
+            ++rec.numCorrectHacks;
+            rec.timeCorrectHacks += time;
+            rec.bestTime = time;
+        }
+        return rec;
+    };
+    GlyphRecord.fromString = function(str){
+        var rec = new GlyphRecord();
+
+        var arr = str.split(" ");
+        rec.numHacks = parseInt(arr[0]);
+        rec.numCorrectHacks = parseInt(arr[1]);
+        rec.timeCorrectHacks = parseInt(arr[2]);
+        rec.bestTime = parseInt(arr[3]) || WORST_TIME;
+        if(rec.numHacks >= 0 &&
+           rec.numCorrectHacks >= 0 && rec.numCorrectHacks <= rec.numHacks &&
+           rec.timeCorrectHacks >= 0 &&
+           rec.bestTime >= 0 && rec.bestTime <= WORST_TIME){
+            return rec;
+        }
+        else{
+            return null;
+        }
+    };
+
+
+    /**
+     * 期間毎の成績を記録するクラス。
+     *
+     * 成績管理方針:
+     * - 一日毎の成績を最大30日分保持する。30日を過ぎたものは捨てていく。
+     * - 通算の成績を保持する。
+     * - All, Month(今日を含めて過去30日分), Week(今日を含めて過去7日分), Today(今日0時から現在まで)の成績を提供する。
+     */
+    function RecordHistory(argAll, argDaily)
+    {
+        var MAX_DAYS = 30;
+        var daily = argDaily || [];
+        var all = argAll || null;
+
+        this.addRecord = addRecord;
+        function addRecord(record)
+        {
+            if(daily.length == 0){
+                daily.push(record.clone());
+            }
+            else if(!daily[0]){
+                daily[0] = record.clone();
+            }
+            else{
+                daily[0].addRecord(record);
+            }
+            if(!all){
+                all = record.clone();
+            }
+            else{
+                all.addRecord(record);
+            }
+        }
+        this.rotateDays = rotateDays;
+        function rotateDays(n)
+        {
+            if(n >= 0 && n < MAX_DAYS){
+                for(var i = 0; i < n; ++i){
+                    daily.splice(0, 0, null);
+                }
+                if(daily.length > MAX_DAYS){
+                    daily.splice(MAX_DAYS, daily.length - MAX_DAYS);
+                }
+            }
+            else if(n >= MAX_DAYS){
+                daily.splice(0, daily.length);
+            }
+        }
+        this.getAll = getAll;
+        function getAll(){
+            return all ? all.clone() : null;
+        }
+        this.getToday = getToday;
+        function getToday(){
+            return daily[0] ? daily[0].clone() : null;
+        }
+        this.getWeek = getWeek;
+        function getWeek(){
+            return getLastDays(7);
+        }
+        this.getMonth = getMonth;
+        function getMonth(){
+            return getLastDays(30);
+        }
+        this.getLastDays = getLastDays;
+        function getLastDays(n){
+            var rec = null;
+            for(var i = 0; i < n; ++i){
+                if(daily[i]){
+                    if(!rec){
+                        rec = daily[i].clone();
+                    }
+                    else{
+                        rec.addRecord(daily[i]);
+                    }
+                }
+            }
+            return rec;
+        }
+        this.toString = toString;
+        function toString(){
+            var str = (all ? all.toString() : "null");
+            for(var i = 0; i < daily.length; ++i){
+                str += "," + (daily[i] ? daily[i].toString() : "null");
+            }
+            return str;
+        }
+    }
+    RecordHistory.fromString = function(str, RecordClass){
+        var arr = str.split(",");
+        var all = RecordClass.fromString(arr[0]);
+        var daily = [];
+        for(var i = 1; i < arr.length; ++i){
+            daily.push(RecordClass.fromString(arr[i]));
+        }
+        return new RecordHistory(all, daily);
+    };
+
+    function IndexedRecordHistory(argDic)
+    {
+        var dic = argDic || {};
+
+        this.get = get;
+        function get(key){
+            return dic[key];
+        }
+        this.add = add;
+        function add(key, rec){
+            var hist = dic[key] || (dic[key] = new RecordHistory());
+            hist.addRecord(rec);
+        }
+        this.rotateDays = rotateDays;
+        function rotateDays(n){
+            for(var key in dic){
+                var hist = dic[key];
+                hist.rotateDays(n);
+            }
+        }
+        this.toString = toString;
+        function toString(){
+            var str = "";
+            for(var key in dic){
+                var hist = dic[key];
+
+                if(str != ""){
+                    str += ":";
+                }
+                str += key + ":" + hist.toString();
+            }
+            return str;
+        }
+    }
+    IndexedRecordHistory.fromString = function(str, RecordClass){
+        var arr = str.split(":");
+        if(arr.length % 2 != 0){
+            return null;
+        }
+        var dic = {};
+        for(var i = 0; i < arr.length; i += 2){
+            dic[arr[i]] = RecordHistory.fromString(arr[i+1], RecordClass);
+        }
+        return new IndexedRecordHistory(dic);
+    };
+
+
+    // Sequence Record
+
+    var dicSequences = new IndexedRecordHistory();
+
+    function toSeqKey(level, glyphCodes)
+    {
+        return level + "/" + glyphCodes.join("/");
+    }
+
+    function addSequenceRecord(level, glyphCodes, arrCorrect, time)
+    {
+        var key = toSeqKey(level, glyphCodes);
+        var numCorrectGlyphs = arrCorrect.filter(function(c){return c;}).length;
+        dicSequences.add(key, SequenceRecord.fromHackResult(numCorrectGlyphs, time, level));
+    }
+    function getSequenceRecord(level, glyphCodes)
+    {
+        return dicSequences.get(toSeqKey(level, glyphCodes));
+    }
+
+    // Glyph Record
+
+    var dicGlyphs = new IndexedRecordHistory();
+
+    function addGlyphRecord(glyphCode, correct, time)
+    {
+        dicGlyphs.add(glyphCode.toLowerCase(), GlyphRecord.fromHackResult(correct, time));
+    }
+    function getGlyphRecord(glyphCode)
+    {
+        return dicGlyphs.get(glyphCode.toLowerCase());
+    }
+
+    // Time Management
+
+    var lastDate = null;
+
+    function updateCurrentTime()
+    {
+        var t = new Date();
+        var currDate = (new Date(t.getFullYear(), t.getMonth(), t.getDate())).getTime();
+
+        var deltaDays = lastDate ? Math.max(0, Math.floor((currDate - lastDate) / (24*60*60*1000))) : 0;
+        if(deltaDays > 0){
+            dicSequences.rotateDays(deltaDays);
+            dicGlyphs.rotateDays(deltaDays);
+        }
+        lastDate = currDate;
+        return deltaDays;
+    }
+
+
+    // Serialize
+
+    function toString()
+    {
+        return lastDate + "'" +
+            dicSequences.toString() + "'" +
+            dicGlyphs.toString();
+    }
+    function loadFromString(str)
+    {
+        var arr = str.split("'");
+        var ld = parseInt(arr[0]) || null;
+        var seq = (arr[0] && IndexedRecordHistory.fromString(arr[1], SequenceRecord)) || null;
+        var glp = (arr[1] && IndexedRecordHistory.fromString(arr[2], GlyphRecord)) || null;
+
+        if(seq && glp){
+            lastDate = ld;
+            dicSequences = seq;
+            dicGlyphs = glp;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    var STORAGE_KEY = "IngressGlyphTools.glyphGameRecord";
+    var loaded = false;
+    function load()
+    {
+        if(loaded){
+            return;
+        }
+        if(!window.localStorage){
+            return;
+        }
+        loaded = true;
+        if(localStorage.getItem(STORAGE_KEY)){
+            if(!loadFromString(localStorage.getItem(STORAGE_KEY))){
+                console.log("Error: Failed to load.");
+            }
+        }
+        updateCurrentTime();
+    }
+
+    function save()
+    {
+        if(!window.localStorage){
+            return;
+        }
+        localStorage.setItem(STORAGE_KEY, toString());
+    }
+
+    function exportRecord(){
+        return toString();
+    }
+    function importRecord(str){
+        if(loadFromString(str)){
+            save();
+            updateCurrentTime();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+})();
---
+++ glyph-tools-ui.js
@@ -0,0 +1,334 @@
+/*
+ * Copyright (c) 2015 gm9
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+(function(){
+    var igt = gm9.IngressGlyphTools;
+
+    // Export:
+    igt.ui = {
+        // Functions
+        putButton: putButton,
+        openTextDialog: openTextDialog,
+        // Classes
+        DataTable: DataTable,
+        DataTableRow: DataTableRow,
+        DataTableColumnSelector: DataTableColumnSelector,
+        RecordRangeSelector: RecordRangeSelector
+    };
+
+    // -------------------------------------------------
+    // UI Library
+    // -------------------------------------------------
+    function putButton(parent, text, onclick){
+        ///@todo 見た目をIngress風にする。
+        var button = document.createElement("input");
+        button.style.marginRight = "0.5em";
+        button.type = "button";
+        button.value = text;
+        button.addEventListener("click", function(e){onclick();}, false);
+        parent.appendChild(button);
+    }
+
+    function openTextDialog(text, okFunc){
+        var dlg = document.createElement("div");
+        var textArea = document.createElement("textarea");
+        textArea.value = text || "";
+        dlg.appendChild(textArea);
+
+        var bottomBar = document.createElement("div");
+        dlg.appendChild(bottomBar);
+
+        function close(){
+            if(dlg.parentNode){
+                dlg.parentNode.removeChild(dlg);
+            }
+        }
+        if(okFunc){
+            putButton(bottomBar, "OK", function(){
+                okFunc(textArea.value);
+                close();
+            });
+        }
+        putButton(bottomBar, "Close", close);
+
+        dlg.style.position = "fixed";
+        dlg.style.left = "0";
+        dlg.style.top = "0";
+        dlg.style.right = "0";
+        dlg.style.bottom = "0";
+        dlg.style.background = "rgba(128,128,128,0.5)";
+        dlg.style.padding = "1em";
+        document.body.appendChild(dlg);
+    }
+
+    // -------------------------------------------------
+    // Data Table UI Library
+    // -------------------------------------------------
+    function DataTable(columns){
+        var element = document.createElement("table");
+        element.setAttribute("border", "1");
+
+        var sortOrderColumnIndex = 0;
+        var sortAscendant = true;
+        var dataRows = [];
+
+        // Header
+        var headerTr = document.createElement("tr");
+        var headerThs = [];
+        for(var ci = 0; ci < columns.length; ++ci){
+            var th = document.createElement("th");
+            th.style.display = columns[ci].visible ? "" : "none";
+            th.appendChild(document.createTextNode(columns[ci].text));
+            headerThs.push(th);
+            headerTr.appendChild(th);
+
+            (function(columnIndex){
+                th.addEventListener("click", function(e){
+                    if(columnIndex == sortOrderColumnIndex){
+                        sortAscendant = !sortAscendant;
+                    }
+                    else{
+                        sortOrderColumnIndex = columnIndex;
+                        sortAscendant = true;
+                    }
+                    updateTableHeaderSortMark();
+                    sortDataRows();
+                }, false);
+            })(ci);
+        }
+        element.appendChild(headerTr);
+
+        // Sort Mark
+        var sortMark = document.createElement("span");
+        function updateTableHeaderSortMark(){
+            if(sortMark.parentNode){
+                sortMark.parentNode.removeChild(sortMark);
+            }
+            while(sortMark.firstChild){
+                sortMark.removeChild(sortMark.firstChild);
+            }
+            sortMark.appendChild(document.createTextNode(sortAscendant ? "↓" : "↑"));
+            headerThs[sortOrderColumnIndex].appendChild(sortMark);
+        }
+        updateTableHeaderSortMark();
+
+        // Column
+        function setColumnVisible(columnIndex, visible){
+            columns[columnIndex].visible = visible; ///@todo ここで変更すべきか、ColumnSelectorで変更すべきか。columnsとColumnSelectorとTableが1対1対多になることがあるのが問題。
+            headerThs[columnIndex].style.display = visible ? "" : "none";
+            dataRows.forEach(function(r){
+                r.setColumnVisible(columnIndex, visible);
+            });
+        }
+
+        // Row
+        function addRow(row){
+            element.appendChild(row.getTr());
+            for(var ci = 0; ci < columns.length; ++ci){
+                row.addColumn(columns[ci].formatter, columns[ci].visible);
+            }
+            row.update();
+            dataRows.push(row);
+        };
+
+        function updateAllRows(){
+            dataRows.forEach(function(r){r.update();});
+        }
+        function sortDataRows(){
+            dataRows.forEach(function(r){r.getTr().parentNode.removeChild(r.getTr());});
+            dataRows.sort(makeDataTableRowComparator(columns, sortOrderColumnIndex, sortAscendant));
+            dataRows.forEach(function(r){element.appendChild(r.getTr());});
+        }
+
+        // Public Properties
+        this.element = element;
+        this.sort = sortDataRows;
+        this.updateAllRows = updateAllRows;
+        this.addRow = addRow;
+        this.setColumnVisible = setColumnVisible;
+        this.getDataRows = function(){return dataRows;};
+    }
+    DataTable.formatter = {
+        number: function(value){return value.toString();},
+        percentage1: function(rate){return (Math.floor(rate*1000)/10).toFixed(1) + "%";},
+        averageCount1: function(count){return (Math.floor(count*10)/10).toFixed(1);},
+        seconds1: function(sec){return sec.toFixed(3) + "s";}
+    };
+
+    function DataTableRow(source){
+        var tr = document.createElement("tr");
+        this.getTr = function(){ return tr;};
+        var cols = [];
+
+        this.addColumn = function(formatter, visible){
+            var columnIndex = cols.length;
+            var td = document.createElement("td");
+            td.style.display = visible ? "" : "none";
+            tr.appendChild(td);
+            cols.push({td:td, formatter:formatter, source:null, value:undefined});
+        };
+
+        this.getColumnValue = getColumnValue;
+        function getColumnValue(columnIndex){
+            return (columnIndex >= 0 && columnIndex < cols.length) ?
+                cols[columnIndex].value : undefined;
+        }
+
+        function setColumnValue(columnIndex, value){
+            if(columnIndex >= 0 && columnIndex < cols.length){
+                var col = cols[columnIndex];
+                if(value != col.value){
+                    var td = col.td;
+                    while(td.firstChild){
+                        td.removeChild(td.firstChild);
+                    }
+                    col.value = value;
+                    var formatted = col.formatter ? col.formatter(value) : value.toString();
+                    if(typeof(formatted) == "string"){
+                        td.appendChild(document.createTextNode(formatted));
+                    }
+                    else{
+                        td.appendChild(formatted);
+                    }
+                }
+            }
+        }
+        this.setColumnVisible = setColumnVisible;
+        function setColumnVisible(columnIndex, visible){
+            if(columnIndex >= 0 && columnIndex < cols.length){
+                cols[columnIndex].td.style.display = visible ? "" : "none";
+            }
+        }
+
+        this.update = function(){
+            var values = source();
+            for(var i = 0; i < values.length; ++i){
+                setColumnValue(i, values[i]);
+            }
+        };
+    }
+    function makeDataTableRowComparator(columns, columnIndex, ascendant){
+        return function(lhs, rhs){
+            var comp = columns[columnIndex].comparator || defaultComparator;
+            var lv = lhs.getColumnValue(columnIndex);
+            var rv = rhs.getColumnValue(columnIndex);
+            var delta = comp(lv, rv);
+            if(delta == 0){
+                var comp0 = columns[0].comparator || defaultComparator;
+                var lv0 = lhs.getColumnValue(0);
+                var rv0 = rhs.getColumnValue(0);
+                delta = comp0(lv0, rv0);
+            }
+            return ascendant ? delta : -delta;
+        };
+    }
+    function defaultComparator(lhs, rhs){
+        return lhs < rhs ? -1 : lhs > rhs ? 1 : 0;
+    }
+
+
+    function DataTableColumnSelector(storageKey, columns, onChangeColumnVisible)
+    {
+        var targetTable = null;
+        this.setTargetTable = setTargetTable;
+        function setTargetTable(t){
+            targetTable = t;
+        }
+
+        function loadSettings(){
+            if(window.localStorage){
+                var checkedStr = localStorage.getItem(storageKey);
+                if(checkedStr){
+                    for(var ci = 0; ci < checkedStr.length && ci < columns.length; ++ci){
+                        columns[ci].visible = (checkedStr.charAt(ci) == "1");
+                    }
+                }
+            }
+        }
+        loadSettings();
+        function saveSettings(){
+            if(window.localStorage){
+                localStorage.setItem(storageKey, columns.map(function(c){return c.visible ? "1" : "0";}).join(""));
+            }
+        }
+
+        var element = document.createElement("p");
+        element.style.lineHeight = "1.5";
+        this.element = element;
+
+        for(var ci = 0; ci < columns.length; ++ci){
+            (function(){
+                var columnIndex = ci;
+                var checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                checkbox.checked = columns[ci].visible;
+                checkbox.addEventListener("click", function(e){
+                    var visible = checkbox.checked;
+                    if(onChangeColumnVisible){
+                        onChangeColumnVisible(columnIndex, visible);
+                    }
+                    if(targetTable){
+                        targetTable.setColumnVisible(columnIndex, visible);
+                    }
+                    columns[columnIndex].visible = visible; ///@todo ここで変更すべきか、ColumnSelectorで変更すべきか。columnsとColumnSelectorとTableが1対1対多になることがあるのが問題。
+                    saveSettings();
+                }, false);
+                var label = document.createElement("label");
+                label.style.display = "inline-block";
+                label.style.paddingRight = "0.5em";
+                label.appendChild(checkbox);
+                label.appendChild(document.createTextNode(columns[ci].text));
+                element.appendChild(label);
+            })();
+        }
+    }
+
+    // -------------------------------------------------
+    // UI Library for glyph-game-record.js
+    // -------------------------------------------------
+
+    function RecordRangeSelector(storageKey, onChange){
+        var sel = document.createElement("select");
+        var ranges = [
+            {value:0, text:"All"},
+            {value:1, text:"Today"},
+            {value:7, text:"Week"},
+            {value:30, text:"Month"}];
+        ranges.forEach(function(optData){
+            var opt = document.createElement("option");
+            opt.value = optData.value;
+            opt.appendChild(document.createTextNode(optData.text));
+            sel.appendChild(opt);
+        });
+        function loadSettings(){
+            if(window.localStorage){
+                var p = parseInt(localStorage.getItem(storageKey));
+                if(p >= 0 && p < sel.options.length){
+                    sel.selectedIndex = p;
+                }
+            }
+        }
+        function saveSettings(){
+            if(window.localStorage){
+                window.localStorage.setItem(storageKey, sel.selectedIndex);
+            }
+        }
+        sel.addEventListener("change", function(e){
+            saveSettings();
+            if(onChange){
+                onChange();
+            }
+        }, false);
+
+        // Public Properties
+        this.element = sel;
+        this.getSelectedRangeDays = function(){
+            return ranges[sel.selectedIndex].value;
+        };
+    }
+
+
+})();
