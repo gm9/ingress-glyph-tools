34451c3d148010ecb36bc5c8c7d9e39063b7b2cc
gm9 <ko4akiyama@gmail.com>
2017-01-07 17:16:46 +0900

    Add new glyph sequences discovered 20170106

--- glyph-sequence-dic.js
+++ glyph-sequence-dic.js
@@ -7,19 +7,31 @@
 (function(){
     // import
     var igt = gm9.IngressGlyphTools;
+
+
+    //
+    // Discovered:
+    // - 20151120
+    // - 20161029
+    // - 20170106
+
+
     // レベル毎のグリフシーケンス
     // 小文字は読み方が曖昧な(自信がない)グリフ
     var SEQUENCES = [
         // Levels Discovered, Date Discovered, Sequence
         //L0
         //L1
+        [0], 20170106, ["adapt"],
         [0], 0, ["advance"],
+        [0], 20170106, ["after"],
         [0], 0, ["again"],
         [0], 0, ["all"],
         [0], 0, ["answer"],
         [0], 0, ["attack"],
         [0], 0, ["avoid"],
         [0], 0, ["barrier"],
+        [0], 20170106, ["before"],
         [0], 0, ["body"],
         [0], 0, ["breathe"],
         [0], 0, ["capture"],
@@ -67,6 +79,7 @@
         [0], 0, ["improve"],
         [0], 0, ["impure"],
         [0], 0, ["inside"],
+        [0], 20170106, ["interrupt"],
         [0], 0, ["journey"],
         [0], 0, ["lead"],
         [0], 0, ["less"],
@@ -76,6 +89,7 @@
         [0], 0, ["message"],
         [0], 0, ["mind"],
         [0], 0, ["more"],
+        [0], 20170106, ["mystery"],
         [0], 0, ["nature"],
         [0], 0, ["new"],
         [0], 0, ["nourish"],
@@ -111,13 +125,16 @@
         [0], 0, ["together"],
         [0], 0, ["truth"],
         [0], 0, ["use"],
+        [0], 20170106, ["victory"],
         [0], 0, ["want"],
         [0], 0, ["weak"],
         [0], 0, ["xm"],
+        [0], 20170106, ["you"],
         //L2
         //L3
         //L4
         // http://www.ingress.tv/2-glyph-hack-sequences.html
+        [2], 20170106, ["ADAPT", "TECHNOLOGY"], //https://fevgames.net/happy-new-glyph-year/
         [2], 0, ["all", "chaos"],
         [2], 0, ["attack", "chaos"],
         [2], 0, ["attack", "difficult"],
@@ -127,6 +144,7 @@
         [2], 0, ["discover", "lie"],
         [2], 0, ["discover", "portal"],
         [2], 0, ["discover", "safety"],
+        [2], 20170106, ["Interrupt", "Evolution"], //https://fevgames.net/happy-new-glyph-year/
         [2], 0, ["lead", "enlightenment"],
         [2], 0, ["liberate", "xm"],
         [2], 0, ["nourish", "journey"],
@@ -141,6 +159,7 @@
         [2], 0, ["search", "past"],
         [2], 0, ["search", "potential"],
         [2], 0, ["seperate", "war"],
+        [2], 20170106, ["YOU", "ADAPT"], //https://fevgames.net/happy-new-glyph-year/
         //L5
         //L6
         [5], 0, ["ACCEPT", "HUMAN", "WEAK"],
@@ -150,6 +169,8 @@
         [5], 0, ["AVOID", "CHAOS", "SOUL"],
         [5], 0, ["AVOID", "DESTINY", "LIE"],
         [5], 0, ["AVOID", "PURE", "CHAOS"],
+        [5], 20170106, ["CAPTURE", "VICTORY", "TOGETHER"], //https://fevgames.net/happy-new-glyph-year/
+        [5], 20170106, ["CLEAR", "YOUR", "MIND"], //https://fevgames.net/happy-new-glyph-year/
         [5], 0, ["COURAGE", "DESTINY", "REBEL"],
         [5], 0, ["DANGER", "CHANGE", "PAST"],
         [5], 0, ["DESTROY", "DIFFICULT", "BARRIER"],
@@ -166,6 +187,7 @@
         [5], 0, ["IMPROVE", "ADVANCE", "PRESENT"],
         [5], 0, ["IMPROVE", "HUMAN", "SHAPERS"],
         [5], 0, ["INSIDE", "XM", "TRUTH"],
+        [5], 20170106, ["INTERRUPT", "ENLIGHTENED", "TECHNOLOGY"], //https://fevgames.net/happy-new-glyph-year/
         [5], 0, ["JOURNEY", "INSIDE", "SOUL"],
         [5], 0, ["LEAD", "ENLIGHTENMENT", "CIVILIZATION"],
         [5], 0, ["LIBERATE", "HUMAN", "FUTURE"],
@@ -175,11 +197,16 @@
         [5], 0, ["QUESTION", "HIDE", "TRUTH"],
         [5], 0, ["REACT", "IMPURE", "CIVILIZATION"],
         [5], 0, ["RETREAT", "SEARCH", "SAFETY"],
+        [5], 20170106, ["TECHNOLOGY", "CAPTURE", "VICTORY"], //https://fevgames.net/happy-new-glyph-year/
+        [5], 20170106, ["TOGETHER", "END", "MYSTERY"], //https://fevgames.net/happy-new-glyph-year/
         [5], 0, ["WAR", "DESTROY", "FUTURE"],
+        [5], 20170106, ["XM", "GROW", "CREATIVITY(1216274548597a9a)"], //https://fevgames.net/happy-new-glyph-year/
         [5], 0, ["XM", "NOURISH", "CIVILIZATION"],
+        [5], 20170106, ["YOU", "HIDE", "CHAOS"], //https://fevgames.net/happy-new-glyph-year/
         //L7
         [7], 0, ["ALL", "CHAOS", "INSIDE", "BODY"],
         [7], 0, ["ATTACK", "WEAK", "SHAPERS", "LIE"],
+        [7], 20170106, ["BEFORE", "MYSTERY", "AFTER", "KNOWLEDGE"], //https://fevgames.net/happy-new-glyph-year/
         [7], 0, ["BREATHE", "AGAIN", "JOURNEY", "AGAIN"],
         [7], 0, ["BREATHE", "NATURE", "PERFECTION", "HARMONY"],
         [7], 0, ["CAPTURE", "FEAR", "DISCOVER", "COURAGE"],
@@ -196,11 +223,15 @@
         [7], 0, ["DEFEND", "MESSAGE", "ANSWER", "IDEA"],
         [7], 0, ["DESTROY", "DESTINY", "HUMAN", "LIE"],
         [7], 0, ["DETERIORATE", "HUMAN", "WEAK", "REBEL"],
+        [7], 20170106, ["ENLIGHTENED", "CAPTURE", "VICTORY", "TOGETHER"], //https://fevgames.net/happy-new-glyph-year/
+        [7], 20170106, ["ESCAPE", "BEFORE", "PURE", "DEATH"], //https://fevgames.net/happy-new-glyph-year/
         [7], 0, ["ESCAPE", "SIMPLE", "HUMAN", "FUTURE"],
         [7], 0, ["FOLLOW", "SHAPERS", "PORTAL", "MESSAGE"],
         [7], 0, ["FORGET", "CONFLICT", "ACCEPT", "WAR"],
         [7], 0, ["HARMONY", "PATH", "NOURISH", "PRESENT"],
+        [7], 20170106, ["HELP", "ENLIGHTENED", "STRONG", "VICTORY"], //https://fevgames.net/happy-new-glyph-year/
         [7], 0, ["HELP", "GAIN", "CREATE", "PURSUE"],
+        [7], 20170106, ["HELP", "RESISTANCE", "STRONG", "VICTORY"], //https://fevgames.net/happy-new-glyph-year/
         [7], 0, ["HIDE", "IMPURE", "HUMAN", "THOUGHT"],
         [7], 0, ["HUMAN", "HAVE", "IMPURE", "CIVILIZATION"],
         [7], 0, ["HUMAN", "PAST", "PRESENT", "FUTURE"],
@@ -209,6 +240,7 @@
         [7], 0, ["IMPROVE", "BODY", "MIND", "SOUL"],
         [7], 0, ["IMPROVE", "BODY", "PURSUE", "JOURNEY"],
         [7], 0, ["INSIDE", "MIND", "JOURNEY", "PERFECTION"],
+        [7], 20170106, ["INTERRUPT", "MESSAGE", "GAIN", "ADVANCE"], //https://fevgames.net/happy-new-glyph-year/
         [7], 0, ["JOURNEY", "not", "IMPROVE", "SOUL"],
         [7], 0, ["LEAD", "PURSUE", "REACT", "DEFEND"],
         [7], 0, ["LESS", "SOUL", "MORE", "MIND"],
@@ -249,6 +281,7 @@
         [8], 20161029, ["ABANDON", "ALL", "TECHNOLOGY", "SAVE", "US"], //https://fevgames.net/new-ingress-glyphs/
         [8], 20161029, ["ABANDON", "FEAR", "DEFEND", "FUTURE", "TOGETHER"], //https://fevgames.net/new-ingress-glyphs/
         [8], 20161029, ["ABANDON", "FEAR", "SEE", "FUTURE", "DESTINATION"], //https://fevgames.net/new-ingress-glyphs/
+        [8], 20170106, ["ADAPT", "THOUGHT", "DISCOVER", "SIMPLE", "SUCCESS"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["ADVANCE", "CIVILIZATION", "PURSUE", "SHAPERS", "PATH"],
         [8], 20161029, ["ANSWER", "N'ZEER", "QUESTION", "POTENTIAL", "KNOWLEDGE"], //https://fevgames.net/new-ingress-glyphs/
         [8], 0, ["ANSWER", "QUESTION", "DISCOVER", "DIFFICULT", "TRUTH"],
@@ -262,6 +295,8 @@
         [8], 0, ["CHAOS", "WAR", "CONFLICT", "DISCOVER", "PEACE"],
         [8], 0, ["civilization", "die", "new", "civilization", "BEGIN"],
         [8], 0, ["clear", "mind", "liberate", "barrier", "BODY"],
+        [8], 20170106, ["CLEAR", "YOUR", "MIND", "CREATIVITY(1216274548597a9a)", "GROW"], //https://fevgames.net/happy-new-glyph-year/
+        [8], 20170106, ["CLEAR", "YOUR", "MIND", "MORE", "BALANCE"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["clearall", "idea", "past", "present", "FUTURE"],
         [8], 0, ["CONTEMPLATE", "FUTURE", "NOT", "SHAPERS", "PATH"],
         [8], 0, ["contemplate", "restraint", "discover", "more", "COURAGE"],
@@ -281,15 +316,19 @@
         [8], 0, ["DISTANCE", "SELF", "AVOID", "HUMAN", "LIE"],
         [8], 0, ["EASY", "PATH", "FUTURE", "FOLLOW", "SHAPERS"],
         [8], 0, ["END", "OLD", "CIVILIZATION", "CREATE", "NEW"],
+        [8], 20170106, ["ESCAPE", "BEFORE", "DEATH", "END", "ALL"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["ESCAPE", "BODY", "JOURNEY", "OUTSIDE", "PRESENT"],
         [8], 0, ["ESCAPE", "BODY", "MIND", "SELF", "WANT"],
         [8], 0, ["FORGET", "WAR", "SEE", "DISTANCE", "HARMONY"],
         [8], 0, ["FORGET", "PAST", "SEE", "PRESENT", "DANGER"],
         [8], 0, ["gain", "truth", "open", "human", "SOUL"],
         [8], 20151120, ["GROW", "UNBOUNDED", "CREATE", "NEW", "FUTURE"],
+        [8], 20170106, ["HAVE", "HARMONY", "TOGETHER", "END", "MYSTERY"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["HARM", "PROGRESS", "PURSUE", "MORE", "WAR"],
         [8], 0, ["HELP", "ENLIGHTENMENT", "CAPTURE", "ALL", "PORTAL"],
+        [8], 20170106, ["help", "gain", "knowledge", "inside", "resistance"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["HELP", "HUMAN", "CIVILIZATION", "PURSUE", "DESTINY"],
+        [8], 20170106, ["HELP", "US", "PROGRESS", "STRONG", "VICTORY"], //https://fevgames.net/happy-new-glyph-year/
         [8], 20161029, ["HELP", "US", "SAVE", "US", "ALL"], //https://fevgames.net/new-ingress-glyphs/
         [8], 0, ["help", "resistance", "capture", "all", "PORTAL"],
         [8], 0, ["HIDE", "STRUGGLE", "ADVANCE", "STRONG", "TOGETHER"],
@@ -301,11 +340,14 @@
         [8], 0, ["IMPERFECT", "TRUTH", "ACCEPT", "COMPLEX", "ANSWER"],
         [8], 0, ["IMPERFECT", "XM", "MESSAGE", "HUMAN", "CHAOS"],
         [8], 0, ["INSIDE", "MIND", "INSIDE", "SOUL", "HARMONY"],
+        [8], 20170106, ["INTERRUPT", "ENLIGHTENED", "TECHNOLOGY", "CAPTURE", "VICTORY"], //https://fevgames.net/happy-new-glyph-year/
+        [8], 20170106, ["interrupt", "message", "lose", "collective", "potential"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["liberate", "portal", "liberate", "human", "MIND"],
         [8], 0, ["liberate", "self", "liberate", "human", "CIVILIZATION"],
         [8], 0, ["LOSE", "SHAPERS", "MESSAGE", "GAIN", "CHAOS"],
         [8], 0, ["MIND", "BODY", "SOUL", "PURE", "HUMAN"],
         [8], 0, ["more", "data", "gain", "portal", "ADVANCE"],
+        [8], 20170106, ["MYSTERY", "BEFORE", "PURE", "KNOWLEDGE", "AFTER"], //https://fevgames.net/happy-new-glyph-year/
         [8], 0, ["old", "nature", "less", "strong", "present"],
         [8], 0, ["past", "chaos", "create", "future", "HARMONY"],
         [8], 0, ["PAST", "PATH", "CREATE", "FUTURE", "JOURNEY"],
@@ -345,7 +387,8 @@
         [8], 0, ["weak", "human", "destiny", "destroy", "CIVILIZATION"],
         [8], 0, ["XM", "CREATE", "COMPLEX", "HUMAN", "DESTINY"],
         //[8], 0, ["XM", "create", "complex", "shapers", "DESTINY"],
-        [8], 0, ["XM", "PATH", "FUTURE", "DESTINY", "HARMONY"]
+        [8], 0, ["XM", "PATH", "FUTURE", "DESTINY", "HARMONY"],
+        [8], 20170106, ["YOU", "HIDE", "CHAOS", "INSIDE", "BODY"] //https://fevgames.net/happy-new-glyph-year/
     ];
 
     var INFOS = [];
